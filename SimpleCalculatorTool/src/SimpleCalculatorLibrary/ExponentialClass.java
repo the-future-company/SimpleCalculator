33cd103557e175f3f000a6c9e14834745af99bc0
inahiddemir <i.nahid.demir@gmail.com>
2019-12-29 02:08:27 -0800

    FixedExpoClassBug

--- SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
+++ SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
@@ -2,12 +2,16 @@ package SimpleCalculatorLibrary;
 
 public class ExponentialClass {
 
-	public static int Exp(int a, int b) {
+	public static int Exp(int a, int b){
 		
+		if(a!=0) {
 		int y=1;
 		for(int x=0; x<b; x++) {
 			y = SimpleMultiplication.Multiply(y, a);
 		}
 		return y;
+		}else {
+			throw new IllegalArgumentException("0 to 0th power is illegal");
+		}
 	}
 }
