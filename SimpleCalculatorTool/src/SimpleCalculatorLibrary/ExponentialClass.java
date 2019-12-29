e80c9d1b281a860a70df27cdecf9b5b718f6106f
inahiddemir <55077963+inahiddemir@users.noreply.github.com>
2019-12-29 02:21:17 -0800

    FixedExpoClassBug (#9)

--- SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
+++ SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
@@ -2,15 +2,16 @@ package SimpleCalculatorLibrary;
 
 public class ExponentialClass {
 
-	public static int Exp(int a, int b) {
-		//If 0 to 0 throw error
-		if (a == 0 && b == 0)
-			throw new IllegalArgumentException("Zero to zero-th power is unknown!");
+	public static int Exp(int a, int b){
 		
+		if(a!=0) {
 		int y=1;
 		for(int x=0; x<b; x++) {
 			y = SimpleMultiplication.Multiply(y, a);
 		}
 		return y;
+		}else {
+			throw new IllegalArgumentException("0 to 0th power is illegal");
+		}
 	}
 }
