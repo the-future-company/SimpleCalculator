39332e758b9d0d47db29d98a749662d4191b1fe1
John Yavuzyilmaz <omercanyy@gmail.com>
2019-12-29 01:59:11 -0800

    added exceoption throw for edge case (#8)

--- SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
+++ SimpleCalculatorTool/src/SimpleCalculatorLibrary/ExponentialClass.java
@@ -3,6 +3,9 @@ package SimpleCalculatorLibrary;
 public class ExponentialClass {
 
 	public static int Exp(int a, int b) {
+		//If 0 to 0 throw error
+		if (a == 0 && b == 0)
+			throw new IllegalArgumentException("Zero to zero-th power is unknown!");
 		
 		int y=1;
 		for(int x=0; x<b; x++) {
